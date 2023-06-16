8477a334bffefa8a47189eab5a569893ebec8ff9
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-06-17 00:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //complete this code
 class Person {
-	  constructor(name, age) {
+  constructor(name, age) {
     this._name = name;
     this._age = age;
   }
-	  get name() {
+
+  get name() {
     return this._name;
   }
 
@@ -14,26 +15,17 @@ class Person {
 }
 
 class Student extends Person {
-	  study() {
-    console.log(`${this._name} is studying.`);
+  study() {
+    console.logs(`${this.name} is studying.`);
   }
 }
 
 class Teacher extends Person {
-	  teach() {
-    console.log("Alice is teaching.");
+  teach() {
+    console.logs(`${this.name} is teaching.`);
   }
 }
 
-const person = new Person("John Doe", 30);
-console.log(person.name); // Output: John Doe
-person.age = 35;
-
-const student = new Student("Alice Smith", 20);
-student.study(); // Output: Alice Smith is studying.
-
-const teacher = new Teacher("Mr. Johnson", 45);
-teacher.teach(); // Output: Mr. Johnson is teaching.
 
 // Do not change the code below this line
 window.Person = Person;
